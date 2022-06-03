0737fcbaf6f998ce039c1893f811cb0c2287bdae
Ingo Ruhnke <grumbel@gmail.com>
2022-06-03 14:29:31 +0200

    Add Nix flake build file

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "A command-line tool to manage multiple git repos";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    gita-src.url = "github:nosarthur/gita?ref=v0.16.2";
+    gita-src.flake = false;
+  };
+
+  outputs = { self, nixpkgs, flake-utils, gita-src }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages = rec {
+          default = gita;
+
+          gita = pkgs.python39Packages.buildPythonPackage rec {
+            pname = "gita";
+            version = "0.16.2";
+
+            src = gita-src;
+
+            postUnpack = ''
+              substituteInPlace source/tests/test_main.py \
+                --replace '"gita"' '"source"' \
+                --replace '"gita\n"' '"source\n"' \
+               --replace '"group add gita' '"group add source'
+            '';
+
+            checkPhase = ''
+               git init
+               pytest tests
+            '';
+
+            postInstall = ''
+              installShellCompletion --bash --name gita ${src}/.gita-completion.bash
+              installShellCompletion --zsh --name gita ${src}/.gita-completion.zsh
+            '';
+
+            propagatedBuildInputs = with pkgs; [
+              python39Packages.pyyaml
+              python39Packages.setuptools
+            ];
+
+            nativeBuildInputs = with pkgs; [
+              installShellFiles
+            ];
+
+            checkInputs = with pkgs; [
+              git
+              python39Packages.pytest
+            ];
+
+            meta = with pkgs.lib; {
+              homepage = "https://github.com/nosarthur/gita";
+              license = licenses.mit;
+            };
+          };
+        };
+      }
+    );
+}
