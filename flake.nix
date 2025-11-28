c13defcc57a975f57e8edb93153a2b6ed119e4ff
Ingo Ruhnke <grumbel@gmail.com>
2025-11-28 22:24:16 +0100

    Update to gita 0.16.8.2

--- flake.nix
+++ flake.nix
@@ -5,11 +5,11 @@
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
 
-    gita-src.url = "github:nosarthur/gita?ref=v0.16.6.1";
-    gita-src.flake = false;
+    gita_src.url = "github:nosarthur/gita?ref=v0.16.8.2";
+    gita_src.flake = false;
   };
 
-  outputs = { self, nixpkgs, flake-utils, gita-src }:
+  outputs = { self, nixpkgs, flake-utils, gita_src }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -19,9 +19,9 @@
 
           gita = pkgs.python3Packages.buildPythonPackage rec {
             pname = "gita";
-            version = "0.16.3";
+            version = "0.16.8.2";
 
-            src = gita-src;
+            src = gita_src;
 
             pyproject = true;
             build-system = [ pkgs.python3Packages.setuptools ];
@@ -30,17 +30,21 @@
               substituteInPlace source/tests/test_main.py \
                 --replace '"gita"' '"source"' \
                 --replace '"gita\n"' '"source\n"' \
-               --replace '"group add gita' '"group add source'
+                --replace '"group add gita' '"group add source'
             '';
 
             checkPhase = ''
+               # workaround for "Permission denied: '/homeless-shelter'"
+               export HOME=$TMPDIR
+
                git init
                pytest tests
             '';
 
             postInstall = ''
-              installShellCompletion --bash --name gita ${src}/.gita-completion.bash
-              installShellCompletion --zsh --name gita ${src}/.gita-completion.zsh
+              installShellCompletion --bash --name gita ${src}/auto-completion/bash/.gita-completion.bash
+              installShellCompletion --fish --name gita ${src}/auto-completion/fish/gita.fish
+              installShellCompletion --zsh  --name gita ${src}/auto-completion/zsh/.gita-completion.zsh
             '';
 
             nativeCheckInputs = with pkgs; [
@@ -53,6 +57,7 @@
             ];
 
             propagatedBuildInputs = with pkgs; [
+              python3Packages.argcomplete
               python3Packages.pyyaml
               python3Packages.setuptools
             ];
